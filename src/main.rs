bf5cd2b99f79a83bd1861667697bcedc7aad4e79
watcol <potfman@gmail.com>
2021-02-11 16:11:33 +0900

    Added the main routine.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::process::Command;
+use std::io::{stdin, stderr, Write};
+
 fn main() {
-    println!("Hello, world!");
+    loop {
+        eprint!("$ ");
+        stderr().flush().expect("Failed to flush stderr.");
+
+        let mut s = String::new();
+        stdin().read_line(&mut s).expect("Failed to read stdin");
+
+        let mut s = s.split_whitespace();
+        let mut child = match s.next() {
+            Some("exit") => break,
+            Some(name) => Command::new(name).args(s).spawn().expect("Failed to spawn a process."),
+            None => continue,
+        };
+
+        child.wait().expect("Command wasn't running.");
+    }
 }
