780ec750b82f29ba3036a7e9ed2368ac2d975fde
watcol <potfman@gmail.com>
2021-02-12 16:29:54 +0900

    Handle signals.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,32 @@
 use std::process::Command;
+use std::sync::{Arc, Mutex};
 
-use rustyline::{Editor, error::ReadlineError};
+use signal_hook::{iterator::Signals, consts::signal};
+use nix::sys::signal::{kill, Signal};
+use nix::unistd::Pid;
+use rustyline::{error::ReadlineError, Editor};
+use std::thread;
 
 fn main() {
+    let mut signals = Signals::new(&[signal::SIGINT, signal::SIGTSTP]).unwrap();
+    let child_id: Arc<Mutex<Option<i32>>> = Arc::new(Mutex::new(None));
+
+    let child_signal = Arc::clone(&child_id);
+    thread::spawn(move || {
+        for sig in signals.forever() {
+            let child = child_signal.lock().unwrap();
+            if let Some(id) = *child {
+                match sig {
+                    signal::SIGINT => println!("\nInterrupt"),
+                    signal::SIGTSTP => println!("\nSuspend"),
+                    _ => unreachable!(),
+                }
+
+                kill(Pid::from_raw(id), Signal::SIGINT).unwrap();
+            }
+        }
+    });
+
     let mut readline = Editor::<()>::new();
 
     loop {
@@ -16,14 +40,18 @@ fn main() {
         let mut line = line.split_whitespace();
 
         let mut child = match line.next() {
-                Some("exit") => break,
-                Some(name) => Command::new(name)
-                    .args(line)
-                    .spawn()
-                    .expect("Failed to spawn a process."),
-                None => continue,
-            };
+            Some("exit") => break,
+            Some(name) => Command::new(name)
+                .args(line)
+                .spawn()
+                .expect("Failed to spawn a process."),
+            None => continue,
+        };
+
+        *Arc::clone(&child_id).lock().unwrap() = Some(child.id() as i32);
 
         child.wait().expect("Command wasn't running.");
+
+        *Arc::clone(&child_id).lock().unwrap() = None;
     }
 }
