e0d950ed8e05235e49560ab35ad7ba81a8a51653
watcol <potfman@gmail.com>
2021-02-12 18:54:33 +0900

    Improved the error.

--- src/main.rs
+++ src/main.rs
@@ -26,37 +26,48 @@ impl fmt::Display for Status {
     }
 }
 
-fn safe_waitid(pid: Pid, flag: WaitPidFlag) -> Status {
-    let (code, status) = unsafe {
+fn safe_waitid(pid: Pid, flag: WaitPidFlag) -> nix::Result<Status> {
+    let (code, status, is_error) = unsafe {
         let mut siginfo = std::mem::zeroed();
-        waitid(P_PID, pid.as_raw() as u32, &mut siginfo, flag.bits());
+        let error = waitid(P_PID, pid.as_raw() as u32, &mut siginfo, flag.bits());
         let siginfo = siginfo as siginfo_t;
-        (siginfo.si_code as i32, siginfo.si_status() as i32)
+        (
+            siginfo.si_code as i32,
+            siginfo.si_status() as i32,
+            error == -1,
+        )
     };
 
-    match code {
-        libc::CLD_EXITED => Status::Exited(status),
-        _ => Status::Signaled(Signal::try_from(status).unwrap()),
+    if is_error {
+        return Err(nix::Error::Sys(nix::errno::Errno::last()));
     }
+
+    Ok(match code {
+        libc::CLD_EXITED => Status::Exited(status),
+        _ => Status::Signaled(Signal::try_from(status).expect("Unnexpected signal.")),
+    })
 }
 
 fn main() {
-    let mut signals = Signals::new(&[signal::SIGINT, signal::SIGTSTP]).unwrap();
+    let mut signals =
+        Signals::new(&[signal::SIGINT, signal::SIGTSTP]).expect("Failed to set signals.");
     let child_id: Arc<Mutex<Option<i32>>> = Arc::new(Mutex::new(None));
 
     let child_signal = Arc::clone(&child_id);
     thread::spawn(move || {
         for sig in signals.forever() {
-            let child = child_signal.lock().unwrap();
+            let child = child_signal.lock().expect("Failed to get the child id.");
             if let Some(id) = *child {
                 match sig {
                     signal::SIGINT => {
                         println!("\nInterrupt");
-                        kill(Pid::from_raw(id), Signal::SIGINT).unwrap();
+                        kill(Pid::from_raw(id), Signal::SIGINT)
+                            .expect("Failed to kill the process.");
                     }
                     signal::SIGTSTP => {
                         println!("\nSuspend: {}", id);
-                        kill(Pid::from_raw(id), Signal::SIGSTOP).unwrap();
+                        kill(Pid::from_raw(id), Signal::SIGSTOP)
+                            .expect("Failed to stop the process.");
                     }
                     _ => unreachable!(),
                 }
@@ -71,7 +82,10 @@ fn main() {
             Ok(s) => s,
             Err(ReadlineError::Interrupted) => continue,
             Err(ReadlineError::Eof) => break,
-            Err(e) => Err(e).expect("Failed to read line."),
+            Err(e) => {
+                eprintln!("Failed to read line: {}", e);
+                continue;
+            }
         };
 
         let mut line = line.split_whitespace();
@@ -79,25 +93,51 @@ fn main() {
         let id = match line.next() {
             Some("exit") => break,
             Some("fg") => {
-                let id = line.next().unwrap().parse::<i32>().unwrap();
-                kill(Pid::from_raw(id), Signal::SIGCONT).unwrap();
+                let id = match line.next() {
+                    Some(s) => s,
+                    None => {
+                        eprintln!("\"fg\" requires an process id.");
+                        continue;
+                    }
+                };
+                let id = match id.parse::<i32>() {
+                    Ok(i) => i,
+                    Err(_) => {
+                        eprintln!("Invalid process id: {}", id);
+                        continue;
+                    }
+                };
+                match kill(Pid::from_raw(id), Signal::SIGCONT) {
+                    Ok(_) => {}
+                    Err(_) => {
+                        eprintln!("Failed to continue the process: {}", id);
+                        continue;
+                    }
+                };
                 id
             }
-            Some(name) => Command::new(name)
-                .args(line)
-                .spawn()
-                .expect("Failed to spawn a process.")
-                .id() as i32,
+            Some(name) => match Command::new(name).args(line).spawn() {
+                Ok(child) => child,
+                Err(_) => {
+                    eprintln!("Invalid command: {}", name);
+                    continue;
+                }
+            }
+            .id() as i32,
             None => continue,
         };
 
-        *Arc::clone(&child_id).lock().unwrap() = Some(id);
+        *Arc::clone(&child_id).lock().expect("Failed to get child.") = Some(id);
 
-        println!("{}", safe_waitid(
-            Pid::from_raw(id),
-            WaitPidFlag::WEXITED | WaitPidFlag::WSTOPPED,
-        ));
+        eprintln!(
+            "{}",
+            safe_waitid(
+                Pid::from_raw(id),
+                WaitPidFlag::WEXITED | WaitPidFlag::WSTOPPED,
+            )
+            .expect("Failed to wait the process")
+        );
 
-        *Arc::clone(&child_id).lock().unwrap() = None;
+        *Arc::clone(&child_id).lock().expect("Failed to get child.") = None;
     }
 }
