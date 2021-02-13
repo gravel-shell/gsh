c32c81547681403c73990428135c83e67c924484
watcol <potfman@gmail.com>
2021-02-13 18:56:59 +0900

    Implemented "cd".

--- src/shell.rs
+++ src/shell.rs
@@ -4,6 +4,7 @@ use std::process::Command;
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub enum Cmd {
+    Cd,
     Fg,
     Cmd(String),
 }
@@ -11,6 +12,7 @@ pub enum Cmd {
 impl Cmd {
     pub fn new<T: AsRef<str>>(name: T) -> Self {
         match name.as_ref() {
+            "cd" => Self::Cd,
             "fg" => Self::Fg,
             s => Self::Cmd(s.into()),
         }
@@ -18,6 +20,17 @@ impl Cmd {
 
     pub fn exec(&self, args: Vec<&str>) -> anyhow::Result<Option<Pid>> {
         Ok(match self {
+            Self::Cd => {
+                let path = match args.len() {
+                    0 => std::env::var("HOME").context("Failed to get the home directory.")?,
+                    1 => String::from(args[0]),
+                    _ => anyhow::bail!("Unexpected args number."),
+                };
+
+                std::env::set_current_dir(path).context("Failed to set current dir.")?;
+
+                None
+            }
             Self::Fg => {
                 if args.len() != 1 {
                     anyhow::bail!("Unexpected args number.");
